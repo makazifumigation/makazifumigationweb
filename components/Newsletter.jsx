5754c91401ef9c24da39bdb3d62388b4bc069ac0
lastborncoltd <lastborn.auth@gmail.com>
2025-06-28 17:04:32 +0300

    alfred

--- components/Newsletter.jsx
+++ components/Newsletter.jsx
@@ -29,8 +29,8 @@ const Newsletter = ({ language }) => {
               transition={{ delay: 0.4 }}
             >
               {language === "sw"
-                ? "Makazi humaananisha 'nyumbani' tunajali kila mahali tunapo hudumia kama kwetu. - Kiezera, Mkurugenzi wa Makazi Fumigation"
-                : "Makazi means “home” we treat every space, we serve like our own. - Kiezera, Director Makazi Fumigation"}
+                ? "Makazi humaananisha 'nyumbani' tunajali kila mahali tunapo hudumia kama kwetu. - Kiezera Alfred, Mkurugenzi wa Makazi Fumigation"
+                : "Makazi means “home” we treat every space, we serve like our own. - Kiezera Alfred, Director Makazi Fumigation"}
               {/* <span className="block md:hidden lg:block">
                 {" "}
                 {language === "sw"
